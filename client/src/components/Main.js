21bd83b82222bae61afdc185e18f71d5928084c6
Hector-bit <hectormartinez.2978@gmail.com>
2021-04-15 14:38:46 -0700

    making the sidebar and main page componenets

---
+++ client/src/components/Main.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+function Main(){
+    return(
+        <section className='about_section'>
+            <h2>About</h2>
+            <div className='aboutDiv'>
+                <p className='aboutparagraph'>Hello my name is Hector, I am a highly capable, full stack developer with experience programming everything from Web API's to Front-end development. My diverse background across a wide range of problem domains allows me to approach issues in an all-encompassing way.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Main;
---
+++ client/src/components/Siderbar.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Sidebar(){
+    return(
+        <section className='about_section'>
+            <h2>This should have less of the screen</h2>
+        </section>
+    )
+}
+
+export default Sidebar;
