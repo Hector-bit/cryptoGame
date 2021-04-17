ef2255f5f152cce242d33fa5ea77371915ec31d4
Hector-bit <hectormartinez.2978@gmail.com>
2021-04-17 01:25:52 -0700

    adding components

--- client/src/components/maincomponents/Game.js
+++ client/src/components/maincomponents/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
@@ -10,12 +10,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const [gameInfoSlide, setGameInfoSlide] = useState(false);
+
 export default function Game() {
     const classes = useStyles();
-  
+    
     return (
       <div className={classes.root}>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={setGameInfoSlide => {!gameInfoSlide}}>
           Start Game
         </Button>
       </div>
