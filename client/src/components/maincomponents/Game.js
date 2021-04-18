da112237f0899470462aeebf81e51709ee0ab09d
Hector-bit <hectormartinez.2978@gmail.com>
2021-04-17 22:29:40 -0700

    dropdown menu works

--- client/src/components/maincomponents/Game.js
+++ client/src/components/maincomponents/Game.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+import GameInfo from './GameInfo';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -10,16 +12,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const [gameInfoSlide, setGameInfoSlide] = useState(false);
-
 export default function Game() {
+    const [getInfo, setGetInfo] = useState(false);
     const classes = useStyles();
-    
+
     return (
       <div className={classes.root}>
-        <Button variant="contained" color="primary" onClick={setGameInfoSlide => {!gameInfoSlide}}>
+        <Button variant="contained" color="primary" onClick={() => setGetInfo(!getInfo)}>
           Start Game
         </Button>
+        <GameInfo info={getInfo}></GameInfo>
       </div>
     );
   }
