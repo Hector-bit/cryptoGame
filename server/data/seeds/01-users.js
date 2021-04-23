071075da60a5382af4b9da95b9cbcc830cd3977b
Hector-bit <hectormartinez.2978@gmail.com>
2021-04-23 04:01:15 -0700

    database seems to work now i just have to write the modifiers

--- server/data/seeds/01-users.js
+++ server/data/seeds/01-users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const password = 'password';
 
 exports.seed = function(knex) {
-  return knex('users').insert([
+  return knex('gamers').insert([
     {id: 1, username: 'hector', password: bcrypt.hashSync(password)},
     {id: 2, username: 'tom', password: bcrypt.hashSync(password)}
   ])
